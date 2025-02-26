6b807f84568ce2127003a654cce04a58efb4b119
Gleb <Glebsd22@gmail.com>
2025-02-27 00:23:28 +0300

    task 25 in lab1 updated and tested

--- lab1/task15/src/removing_brackets.py
+++ lab1/task15/src/removing_brackets.py
@@ -12,12 +12,13 @@ class Removing:
         self.closing_brackets = ')}]'
         self.opening_brackets = "({["
         self.stack = {}
+        self.indexes_to_delete = []
 
     def add(self, e, i):
         self.stack[i] = e
 
     def deleter(self, e, index):
-        for i, j in self.stack.items():
+        for i, j in list(self.stack.items())[::-1]:
             if j == '(' and e == ')':
                 del self.stack[i]
                 break
@@ -30,20 +31,56 @@ class Removing:
         else:
             self.add(e, index)
 
+    def unclosed_bracket(self, e):
+        for i, j in list(self.stack.items())[::-1]:
+            if j == '(' and e == ')':
+                self.indexes_to_delete.append(i)
+                break
+            elif j == '[' and e == ']':
+                self.indexes_to_delete.append(i)
+                break
+            elif j == '{' and e == '}':
+                self.indexes_to_delete.append(i)
+                break
+
+    def resulting_function(self):
+        for i in range(len(self.s)):
+            if self.s[i] in self.opening_brackets:
+                self.add(self.s[i], i)
+            elif self.s[i] in self.closing_brackets:
+                for j in range(len(list(self.stack.values()))):
+                    arr = list(self.stack.values())[::-1]
+                    if (arr[j] == '(' and self.s[i] == ')') or (arr[j] == '[' and self.s[i] == ']') or (arr[j] == '{' and self.s[i] == '}'):
+                        self.deleter(self.s[i], i)
+                        break
+                    if self.s[i] == ')' and (arr[j] == "[" or arr[j] == '{'):
+                        self.add(self.s[i], i)
+                        self.unclosed_bracket(self.s[i])
+                        self.deleter(self.s[i],i)
+                        break
+                    if self.s[i] == ']' and (arr[j] == "(" or arr[j] == '{'):
+                        self.add(self.s[i], i)
+                        self.unclosed_bracket(self.s[i])
+                        self.deleter(self.s[i],i)
+                        break
+                    if self.s[i] == '}' and (arr[j] == "[" or arr[j] == '('):
+                        self.add(self.s[i], i)
+                        self.unclosed_bracket(self.s[i])
+                        self.deleter(self.s[i],i)
+                        break # 1 2 3 4 5
+                else:
+                    self.deleter(self.s[i], i)
+        res = ''
+        for i in range(len(self.s)):
+            if i not in self.stack.keys() and i not in self.indexes_to_delete:
+                res += self.s[i]
+        return res
+
 
 def main():
     rem = Removing()
-    for i in range(len(rem.s)):
-        if rem.s[i] in rem.opening_brackets:
-            rem.add(rem.s[i], i)
-        elif rem.s[i] in rem.closing_brackets:
-            rem.deleter(rem.s[i], i)
 
-    res = ''
-    for i in range(len(rem.s)):
-        if i not in rem.stack.keys():
-            res += rem.s[i]
-
+    res = rem.resulting_function()
 
     write_output(15, res)
 
---
+++ lab1/task15/tests/test_removing_brackets.py
@@ -0,0 +1,93 @@
+import unittest
+from lab1.task15.src.removing_brackets import *
+
+class TestRemovingBrackets(unittest.TestCase):
+
+    def setUp(self):
+        self.rem = Removing()
+
+    def test_add_and_exists(self):
+        # given
+        self.s = '(][{()(()))))}]'
+        expected_result = '[{()(())}]'
+
+        # when
+        self.rem.s = self.s
+        result = self.rem.resulting_function()
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_minimum_input(self):
+        # given
+        self.s = '('
+        expected_result = ''  # ничего не остается
+
+        # when
+        self.rem.s = self.s
+        result = self.rem.resulting_function()
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_maximum_input(self):
+        # given
+        self.s = '(){}[]' * 10  # 60 символов (максимум 100)
+        expected_result = '(){}[]' * 10  # все скобки должны остаться
+
+        # when
+        self.rem.s = self.s
+        result = self.rem.resulting_function()
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_unmatched_brackets(self):
+        # given
+        self.s = '([)]'
+        expected_result = '[]'
+
+        # when
+        self.rem.s = self.s
+        result = self.rem.resulting_function()
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_nested_brackets(self):
+        # given
+        self.s = '((()))'
+        expected_result = '((()))'  # Все скобки должны остаться
+
+        # when
+        self.rem.s = self.s
+        result = self.rem.resulting_function()
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_complex_case(self):
+        # given
+        self.s = '{([])}'
+        expected_result = '{([])}'  # Все скобки должны остаться
+
+        # when
+        self.rem.s = self.s
+        result = self.rem.resulting_function()
+
+        # then
+        self.assertEqual(result, expected_result)
+
+    def test_100_elements(self):
+        # given
+        self.s = '([){]]))[{[][)]{(}}){([{][[[[)(()}}]](][}(}}[}{)}(}{)}[()[[]]{][[{){{})[{{))[(({[]][))}})([)([][{{(['
+        expected_result = '[][]()(){}{}()[[]]{}[][]'  # Все скобки должны остаться
+        # when
+        self.rem.s = self.s
+        result = self.rem.resulting_function()
+
+        # then
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
