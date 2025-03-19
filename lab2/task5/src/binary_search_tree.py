8d24ee6b2b08aadf079ecf53ab0dc6af984dea8c
Mitya01 <dimitrimatyukov@yandex.ru>
2025-03-19 17:20:21 +0300

    added lab2 utils and task5

---
+++ lab2/task5/src/binary_search_tree.py
@@ -0,0 +1,105 @@
+from lab2.utils import read_input, write_output, decorate
+
+
+def insert(root, key):
+    if root is None:
+        return {'key': key, 'left': None, 'right': None}
+    if key < root['key']:
+        root['left'] = insert(root['left'], key)
+    elif key > root['key']:
+        root['right'] = insert(root['right'], key)
+    return root
+
+
+def delete(root, key):
+    if root is None:
+        return None
+    if key < root['key']:
+        root['left'] = delete(root['left'], key)
+    elif key > root['key']:
+        root['right'] = delete(root['right'], key)
+    else:
+        if root['left'] is None:
+            return root['right']
+        if root['right'] is None:
+            return root['left']
+        successor = find_min(root['right'])
+        root['key'] = successor['key']
+        root['right'] = delete(root['right'], successor['key'])
+    return root
+
+
+def find_min(node):
+    while node['left'] is not None:
+        node = node['left']
+    return node
+
+
+def exists(root, key):
+    if root is None:
+        return False
+    if key == root['key']:
+        return True
+    return exists(root['left'], key) if key < root['key'] else exists(root['right'], key)
+
+
+def find_next(root, key):
+    result = None
+    while root:
+        if root['key'] > key:
+            result = root['key']
+            root = root['left']
+        else:
+            root = root['right']
+    return result
+
+
+def find_prev(root, key):
+    result = None
+    while root:
+        if root['key'] < key:
+            result = root['key']
+            root = root['right']
+        else:
+            root = root['left']
+    return result
+
+
+def binary_search_tree(lines):
+    root = None
+    output = []
+
+    for line in lines:
+        if not line.strip():
+            continue
+        cmd, x = line.split()
+        x = int(x)
+
+        if cmd == 'insert':
+            root = insert(root, x)
+        elif cmd == 'delete':
+            root = delete(root, x)
+        elif cmd == 'exists':
+            res = exists(root, x)
+            output.append('true' if res else 'false')
+        elif cmd == 'next':
+            res = find_next(root, x)
+            output.append(str(res) if res is not None else 'none')
+        elif cmd == 'prev':
+            res = find_prev(root, x)
+            output.append(str(res) if res is not None else 'none')
+
+    return output
+
+
+def main():
+    data = read_input(5)
+    print(data)
+    res = binary_search_tree(data)
+    print(res)
+    res = '\n'.join(map(str, res))
+    print(res)
+    write_output(5, res)
+
+if __name__ == '__main__':
+    decorate(5, 'binary_search_tree')
---
+++ lab2/utils.py
@@ -0,0 +1,61 @@
+import os
+import time
+import tracemalloc
+import importlib.util
+
+
+def read_input(task):
+    """
+    Функция для чтения входных данных из файла 'input.txt'.
+
+    Возвращает список строк без символов перевода строки.
+    """
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), f'task{task}', 'txtf', 'input.txt'))
+    with open(base_path, 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def write_output(task, *args):
+    """
+    Функция для записи выходных данных в файл 'output.txt'.
+
+    Принимает переменное количество аргументов и записывает каждый на новой строке.
+    """
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), f'task{task}', 'txtf', 'output.txt'))
+    with open(base_path, 'w') as f:
+        for arg in args:
+            print(arg, file=f)
+
+
+def decorate(task, task_name):
+    import time
+    import tracemalloc
+
+    main_path = os.path.abspath(os.path.join(os.path.dirname(__file__), f'task{task}', 'src', task_name))
+    spec = importlib.util.spec_from_file_location("main_module", main_path + '.py')
+    main_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(main_module)
+
+    tracemalloc.start()
+
+    start = time.perf_counter()
+
+    main_module.main()
+
+    end = time.perf_counter()
+
+    print("Время работы: ", end - start, "секунд")
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Пиковая память: {peak / 2 ** 20:.2f} MB")
+    tracemalloc.stop()
+
+
+def check_time_and_memory(task_func, tasknumber, *args):
+    print(f"task №{tasknumber} tests")
+    tracemalloc.start()
+    start_time = time.time()
+
+    task_func(*args)
+
+    tracemalloc.stop()
+    return tracemalloc.get_traced_memory()[1]//1024, time.time() - start_time
